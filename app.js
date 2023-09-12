0459140378523e6f9cf71b0216f5fca6d9577e3c
iurymacks <iury.macks@gmail.com>
2023-09-12 19:57:31 -0300

    first commit node

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: true}));
+// console.log(routes.someText);
+
+
+app.use(adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>')
+})
+
+app.listen(3000);
